2ea186f8a13bbde06b5300a0a68a9b5fa47e1320
Jeremy Bertin <jeremy.bertin76@hotmail.fr>
2020-12-01 22:32:45 +0100

    first commit

---
+++ precache-manifest.9279b33ac87d01edb894f78bdc51c2f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbee2c0262d359050da990ca37930ed3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f5db251be04d81fbc5e",
+    "url": "/static/css/main.0df830f0.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "7f5db251be04d81fbc5e",
+    "url": "/static/js/main.2da81dbe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
